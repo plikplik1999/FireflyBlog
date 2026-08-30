cc4b951f85807fadc4a7a545fb34056274ad3c4e
plikplik1999 <plik_y@outlook.com>
2026-08-30 20:41:16 +0800

    fix: base path for user site

--- astro.config.mjs
+++ astro.config.mjs
@@ -69,7 +69,7 @@ const adapter = process.env.CF_WORKERS
 export default defineConfig({
 	site: "https://plikplik1999.github.io",
 
-	base: "/FireflyBlog/",
+	base: "/",
 	trailingSlash: "always",
 
 	// 字体配置 - 只加载实际使用的字体，跳过未引用的以加快构建
