aca2579d37ad9b09a21f0e454880a56ef592e391
plikplik1999 <plik_y@outlook.com>
2026-08-30 13:54:07 +0800

    fix

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -40,7 +40,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
 			"assets/images/DesktopWallpaper/d1.avif",
-			"assets/images/DesktopWallpaper/d11.avif",			
+			"assets/images/DesktopWallpaper/d2.avif",			
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
